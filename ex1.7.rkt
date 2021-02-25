18ee9148ca48e0b5d4081ee30f87f5d380dc2f25
Andrey Skobelev <v1212186@gmail.com>
2021-02-25 22:30:19 +0300

    ex1.7 additions

--- ex1.7.rkt
+++ ex1.7.rkt
@@ -2,9 +2,14 @@
 (define (square x) (* x x))
 
 (define (sqrt-iter guess x)
-(if (new-good-enough? guess x)
+(if (good-enough? guess x)
 guess
 (sqrt-iter (improve guess x) x)))
+
+(define (new-sqrt-iter guess x)
+(if (new-good-enough? guess x)
+guess
+(new-sqrt-iter (improve guess x) x)))
 
 (define (improve guess x)
 (average guess (/ x guess)))
@@ -24,12 +29,20 @@ guess
 (define (sqrt x)
 (sqrt-iter 1.0 x))
 
+(define (new-sqrt x)
+(new-sqrt-iter 1.0 x))
+
+(new-sqrt 9)
 (sqrt 9)
 
+(new-sqrt 0.0000001)
 (sqrt 0.0000001) ;=~ 0.03125, but 0.03125^2 =~ 0.000976 which is far enough from 0.0000001
 ; value of 0.001 is too big to compute difference between very small values
+
+(new-sqrt 10000000000000)
 (sqrt 10000000000000) ; but for large values 0.001 is too small, because floating point precision cannot
 ; represent such small difference between large values. that means that improving guess cannot yield new value that has
 ; 0.001 difference from old value
 ; the result is infinite recursion
+
 
